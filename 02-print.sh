4e8cff79372d43d2a317519bfdf277fd4fecfff9
ZiSingh <zitendernegi@gmail.com>
2023-05-25 13:43:34 +0530

    print a file

---
+++ 02-print.sh
@@ -0,0 +1,12 @@
+#to print a message there are couple of commands but echo is widely used one
+echo Hello World
+#while printing some times to grab the attention of user we might need to print in some color
+#syntax : echo -e "\e[COLmMESSAGE\e[0M"
+
+
+echo -e "\e[31mHello in Red Colour \e[0m"
+echo -e "\e[32mHello in Green Colour \e[0m"
+echo -e "\e[33mHello in Yellow Colour \e[0m"
+echo -e "\e[34mHello in Blue Colour \e[0m"
+echo -e "\e[35mHello in Magneta Colour \e[0m"
+echo -e "\e[36mHello in Cyan Colour \e[0m"
